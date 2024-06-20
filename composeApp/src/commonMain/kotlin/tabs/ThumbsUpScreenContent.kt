079e26d3b4c728b995a45915d22940da0fa452eb
Theodoro Mota <theolm.mota@gmail.com>
2024-06-20 17:51:35 -0300

    Updating demo

--- composeApp/src/commonMain/kotlin/tabs/ThumbsUpScreenContent.kt
+++ composeApp/src/commonMain/kotlin/tabs/ThumbsUpScreenContent.kt
@@ -2,15 +2,20 @@ package tabs
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.bringToFront
+import navigation.LocalNavigator
+import navigation.configuration.FeatureAConfig
 
 @Composable
 internal fun ThumbsUpContent(componentContext: ComponentContext) {
+    val navigator = LocalNavigator.current
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -23,6 +28,16 @@ internal fun ThumbsUpContent(componentContext: ComponentContext) {
     ) {
         Box(Modifier.fillMaxSize()) {
             Text("ThumbsUp")
+
+            Button(
+                onClick = {
+                    navigator?.apply {
+                        navigation.bringToFront(FeatureAConfig.HomeA)
+                    }
+                }
+            ) {
+                Text("Go to Feature A Home")
+            }
         }
     }
 }
